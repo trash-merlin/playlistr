5dc0103aa82e59790a5d8b413d28dbeb4c00ef7b
Michael Busbee <mbusbee404@icloud.com>
2021-05-21 13:38:50 -0400

    Added basic structure for app and file structure

---
+++ src/Components/App/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>play<span className="highlight">List</span>r</h1>
+      <div className="App">
+      {/*<!-- Add a SearchBar component -->*/}
+    <div className="App-playlist">
+      {/*<!-- Add a SearchResults component -->*/}
+      {/*<!-- Add a Playlist component --> */}
+    </div>
+  </div>
+    </div>
+  );
+}
+
+export default App;
